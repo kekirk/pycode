940a77a858e4863de34656a2ab7f447fc786ca8a
Kirk Chen <kekirk@163.com>
2018-11-23 00:14:55 +0800

    test6

---
+++ YCD/lesson-6/test-6-2.py
@@ -0,0 +1,24 @@
+i=0
+while i<=5:
+	j=0
+	while j<=i:
+		# print("*",j,end="")
+		print("*",end="")
+		j+=1
+		pass	
+	i+=1
+	print("")
+	pass	
+else:
+	print("END")
+
+i=1
+while i <=9:
+	j=1
+	while j<=i:
+		print(j,"*",i,"=",i*j," ",end="")
+		j+=1
+		pass
+	i+=1
+	print("")
+	pass
